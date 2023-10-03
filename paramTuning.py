04569a8f16ed6906899935725491c0006bf32696
vaishnavi026 <f2015777@pilani.bits-pilani.ac.in>
2023-10-02 22:18:55 -0500

    changed python3

--- paramTuning.py
+++ paramTuning.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 def run_training_and_capture_output(num_head, num_layer, max_iters=500):
-    command = ["python", "train.py", "config/train_shakespeare_char.py", f"--n_head={num_head}", f"--n_layer={num_layer}"]
+    command = ["python3", "train.py", "config/train_shakespeare_char.py", f"--n_head={num_head}", f"--n_layer={num_layer}"]
 
     result = subprocess.run(command, text=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout = result.stdout  # This captures the standard output without the warnings
